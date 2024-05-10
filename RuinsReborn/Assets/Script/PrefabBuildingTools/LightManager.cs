c86b19361546bb12059a7f5dfb0575bf69ec4574
Julez137 <julianfourie99@gmail.com>
2024-05-10 22:01:06 +0200

    Finished Brick house prefab

---
+++ RuinsReborn/Assets/Script/PrefabBuildingTools/LightManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    public bool enableLights;
+    Light[] lights;
+    LightManager[] managerChildren;
+    private void OnValidate()
+    {
+        managerChildren = GetComponentsInChildren<LightManager>();
+
+        if (managerChildren != null)
+        {
+            foreach (var manager in managerChildren)
+                manager.EnableLights(enableLights);
+        }
+        else
+        {
+            lights = GetComponentsInChildren<Light>();
+            EnableLights(enableLights);
+        }
+    }
+
+    public void EnableLights(bool isEnabled)
+    {
+        enableLights = isEnabled;
+        foreach (var light in lights)
+                light.enabled = enableLights;
+    }
+}
