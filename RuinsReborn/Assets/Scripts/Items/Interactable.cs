d30e518032824e2d2eadfdc959683e8a705475cb
Julez137 <julianfourie99@gmail.com>
2025-02-23 13:41:10 +0200

    Add : Scriptable Object (GlobalItemsDataHolder) to easily assign item data
    # Conflicts:
    #	RuinsReborn/Assets/Scripts/UI/Inventory/EquippedItem.cs

--- RuinsReborn/Assets/Scripts/Items/Interactable.cs
+++ RuinsReborn/Assets/Scripts/Items/Interactable.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public abstract class Interactable : MonoBehaviour
 {
     protected Collider thisCollider;
+    [Tooltip("The name used to reference the gameobject in the scene. This is only to assign the data to the matched object, not for UI.")]
+    public string refName;
     [Tooltip("The name of the object. This will be displayed in UI menus")]
     public string objectName;
-    [Tooltip("The text being displayed when the object can be interactable. Template: Press <Key> to [insert text here].")]
+    [Tooltip("The text being displayed when the object can be interactable. Template: Press <Key> to [interactableText].")]
     public string interactableText;
     [Tooltip("The width of the text displayed. This will control the background of the text to highlight it when displayed.")]
     public float textWidth;
